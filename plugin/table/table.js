f8fb178437a04e739101eb3dcb74e523de29035b
unknown <anuj.mistry24@gmail.com>
2023-09-20 10:36:46 +0530

    remove unused function

--- plugin/table/table.js
+++ plugin/table/table.js
@@ -1,6 +1,6 @@
 import { OuterbasePluginConfig_$PLUGIN_ID } from '../config';
 import { templateTable_$PLUGIN_ID } from './view/table-view';
-import  { Map, tileLayer, featureGroup, icon} from 'leaflet';
+import  { Map, Marker, tileLayer, FeatureGroup, icon, featureGroup, marker} from 'leaflet';
 export class OuterbasePluginTable_$PLUGIN_ID extends HTMLElement {
     static get observedAttributes() {
         return privileges
@@ -20,82 +20,39 @@ export class OuterbasePluginTable_$PLUGIN_ID extends HTMLElement {
     connectedCallback() {
 
                 this.config.tableValue = JSON.parse(this.getAttribute("tableValue"));
-
-                // this.loadExternalScript("https://unpkg.com/leaflet@1.9.4/dist/leaflet.js").then(
-                //     ()=>{
-                        this.useExternalScript()
-                    // }
-                // ).catch((error) => {
-                //     console.error('Error loading external script:', error);
-                // });
+                this.useExternalScript()
 
 
-    }
-
-    loadExternalScript(url) {
-        return new Promise(((resolve, reject) => {
-            const script = document.createElement("script");
-            script.src = url;
-                script.type = "application/javascript",
-                script.onload = () => {
-                    resolve()
-                };
-                script.onerror = () => {
-                    reject(new Error(`Failed to load script: ${url}`))
-                };
-                document.head.appendChild(script)
-        }
-        ))
-    }
-    loadExternalCSSLink(url) {
-        return new Promise(((resolve, reject) => {
-            const link = document.createElement("link");
-            link.href = url;
-                link.rel = "stylesheet";
-                link.onload = () => {
-                    resolve()
-                };
-            
-                link.onerror = () => {
-                    reject(new Error(`Failed to load link: ${url}`))
-                };
-                
-                document.head.appendChild(link);
-        }
-        ))
     }
     useExternalScript() {
 
         var map = this.shadowRoot.getElementById('map');
-        var e =Map(map).setView([51.505, -.09], 13);
+        var renderMap =new Map(map).setView([51.505, -.09], 13);
 
-        // L.markerClusterGroup();
-
-        // var e = L.map("map-to-render").setView([51.505, -.09], 13);
         tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(e);
+        }).addTo(renderMap);
 
-        let markersGroupArray = this.getMarkers(L);
-        
-        
-        if(markersGroupArray.length && markersGroupArray.length > 0){
-            featureGroup(markersGroupArray).addTo(e)
-        }
+        let markersGroupArray = this.getMarkersFromTableValue();
+        featureGroup(markersGroupArray).addTo(renderMap)
+        // if(markersGroupArray.length && markersGroupArray.length > 0){
+        //     featureGroup(markersGroupArray).addTo(renderMap)
+        // }
     }
 
-    getMarkers(L){
+    getMarkersFromTableValue(){
         const tableValue = this.config.tableValue;
 
         if(tableValue.length && tableValue.length != 0){
+            // TODO ADD CUSTOM ICON SUPPORT
             let myIcon = icon({
                 iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
             })
             return tableValue.map((singleColumnValues, index)=>{
                 let lat = singleColumnValues[this.config.latitudeKey];
                 let lng = singleColumnValues[this.config.latitudeKey];
-                return new L.marker([lat, lng]).bindPopup(`${index}, ${lat}, ${lng}`);
+                return marker([lat, lng], {icon : myIcon}).bindPopup(`${index}, ${lat}, ${lng}`);
             })
         }else{
             return [];
