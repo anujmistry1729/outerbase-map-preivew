3e0cddba39239107794d975e6246cd39c62d563e
unknown <anuj.mistry24@gmail.com>
2023-09-23 12:09:54 +0530

    chore: add outerbase event varaibles

---
+++ plugin/outerbase-events.js
@@ -0,0 +1,34 @@
+var OuterbaseEvent = {
+    // The user has triggered an action to save updates
+    onSave: "onSave",
+}
+
+var OuterbaseColumnEvent = {
+    // The user has began editing the selected cell
+    onEdit: "onEdit",
+    // Stops editing a cells editor popup view and accept the changes
+    onStopEdit: "onStopEdit",
+    // Stops editing a cells editor popup view and prevent persisting the changes
+    onCancelEdit: "onCancelEdit",
+    // Updates the cells value with the provided value
+    updateCell: "updateCell",
+}
+
+var OuterbaseTableEvent = {
+    // Updates the value of a row with the provided JSON value
+    updateRow: "updateRow",
+    // Deletes an entire row with the provided JSON value
+    deleteRow: "deleteRow",
+    // Creates a new row with the provided JSON value
+    createRow: "createRow",
+    // Performs an action to get the next page of results, if they exist
+    getNextPage: "getNextPage",
+    // Performs an action to get the previous page of results, if they exist
+    getPreviousPage: "getPreviousPage"
+}
+
+module.exports = {
+    OuterbaseColumnEvent,
+    OuterbaseEvent,
+    OuterbaseTableEvent
+}
