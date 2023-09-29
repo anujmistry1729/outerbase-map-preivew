7992490a374585da9b83da21d3b570c861f02686
Anuj <anuj.mistry24@gmail.com>
2023-09-29 15:06:23 +0530

    add clustering in preview

--- plugin/table/config.js
+++ plugin/table/config.js
@@ -1,7 +1,7 @@
 import {Map, tileLayer, icon , featureGroup, marker, popup} from 'leaflet';
+import {MarkerClusterGroup} from 'leaflet.markercluster/src';
 
 import { OuterbasePluginConfig } from "../config";
-import { renderMapSingleLatLng } from "../map/renderMapSingleLatLng";
 import { templateConfiguration } from "./view/config-view";
 import { ATTRIBUTION, MAX_ZOOM_LEVEL, TILE_LAYER, continentsBoundingBox } from '../constant';
 export class OuterbasePluginTableConfiguration extends HTMLElement {
@@ -11,56 +11,52 @@ export class OuterbasePluginTableConfiguration extends HTMLElement {
 
   config = new OuterbasePluginConfig(JSON.parse(this.getAttribute("configuration")));
   items = JSON.parse(this.getAttribute("tableValue"));
+  previewMarkers = [];
+  clusterMarker = undefined
 
   constructor() {
     super();
-
+    
     //attach shadow clone
     this.shadow = this.attachShadow({ mode: "open" });
     this.shadow.appendChild(templateConfiguration.content.cloneNode(true));
-
-    //get sample data for preview
-    let sample = this.items.length ? this.items[0] : {};
-    let keys = Object.keys(sample);
-    let lat = sample[this.config.latitudeKey];
-    let lng = sample[this.config.longitudeKey];
-
+    
+    const previewMapElement = this.shadowRoot.getElementById('preview-map');
+    const latitudeKeySelect = this.shadow.getElementById("latitudeKeySelect");
+    const longitudeKeySelect = this.shadow.getElementById("longitudeKeySelect");
+    const clusteringInput = this.shadow.querySelector("#is-clustering");
+    const iconURLInputEl = this.shadow.querySelector("#iconURL");
+    const saveButton = this.shadow.getElementById("saveButton");
+    
+    //get sampleData for preview
+    this.sampleData = this.items.length ? this.items[0] : {};
+    this.dataKeys = Object.keys(this.sampleData);
+    let lat = this.sampleData[this.config.latitudeKey];
+    let lng = this.sampleData[this.config.longitudeKey];
+    
     console.log(this.config)
-
+    
     //get options for select tag 
-    let latitudeKeyOptions =   ` + ${keys.map((key) => `<option value="${key}" ${key===this.config.latitudeKey ? 'selected' : '' }>${key}</option>`).join("")} + `;
-    let longitudeKeyOptions = ` + ${keys.map((key) => `<option value="${key}" ${key===this.config.longitudeKey ? 'selected' : '' }>${key}</option>`).join("")} + `;
-
-    //render options 
+    let latitudeKeyOptions =   ` + ${this.dataKeys.map((key) => `<option value="${key}" ${key===this.config.latitudeKey ? 'selected' : '' }>${key}</option>`).join("")} + `;
+    let longitudeKeyOptions = ` + ${this.dataKeys.map((key) => `<option value="${key}" ${key===this.config.longitudeKey ? 'selected' : '' }>${key}</option>`).join("")} + `;
+    
+    //dropdown options 
     this.shadow.querySelector("#latitudeKeySelect").innerHTML =latitudeKeyOptions;
     this.shadow.querySelector("#longitudeKeySelect").innerHTML =longitudeKeyOptions;
-
+    
     //Latitude Select
-    const latitudeKeySelect = this.shadow.getElementById("latitudeKeySelect");
     latitudeKeySelect.addEventListener("change", ()=>{
-
-      //validate latitude key sample value
-      const latitudeLabelEl = this.shadow.querySelector("#latitude-label");
-      latitudeLabelEl.innerHTML = 'Latitude Key';
-      // if(latLongRegex.exec(sample[latitudeKeySelect.value])){
-        this.config.latitudeKey = latitudeKeySelect.value;
-      // }else{
-      //   latitudeLabelEl.innerHTML += '<span style="color:red;"> Please select valid Latitude Key</span>'
-      // }
-      
+      this.config.latitudeKey = latitudeKeySelect.value;
       this.render();
     });
 
     //Longitude Select
-    const longitudeKeySelect = this.shadow.getElementById("longitudeKeySelect");
     longitudeKeySelect.addEventListener("change", ()=>{
       this.config.longitudeKey = longitudeKeySelect.value;
       this.render();
     })
 
     //Clustering Toggle
-    const clusteringInput = this.shadow.querySelector("#is-clustering");
-
     if(this.config.isClustering){
       clusteringInput.checked = this.config.isClustering
     }
@@ -75,7 +71,6 @@ export class OuterbasePluginTableConfiguration extends HTMLElement {
     })
 
     //url input
-    const iconURLInputEl = this.shadow.querySelector("#iconURL");
     if(this.config.iconurl){
       iconURLInputEl.value = this.config.iconurl
     }
@@ -88,7 +83,6 @@ export class OuterbasePluginTableConfiguration extends HTMLElement {
     })
 
     //save and update config keys
-    const saveButton = this.shadow.getElementById("saveButton");
     saveButton.addEventListener("click", () => {
       console.log("save button");
       console.log(this.config.toJSON())
@@ -99,7 +93,6 @@ export class OuterbasePluginTableConfiguration extends HTMLElement {
     });
 
     
-    const previewMapElement = this.shadowRoot.getElementById('preview-map');
     
     // let previewMap = renderMapSingleLatLng(previewMapElement, lat, lng);
     this.previewMap = new Map(previewMapElement);
@@ -120,12 +113,84 @@ export class OuterbasePluginTableConfiguration extends HTMLElement {
 
 
   render() {
-    
-    
 
-
+    if(this.config.latitudeKey && this.config.longitudeKey){
+      this.clearMarkers();
+      console.log("after clear", this.previewMarkers)
+      this.addMarkers()
+      console.log("after add", this.previewMarkers)
+    }
     
   }
+
+
+  clearMarkers() {
+    this.previewMarkers.forEach(marker => {
+        marker.removeFrom(this.previewMap);
+    });
+    if(this.clusterMarker != null){
+      this.previewMap.removeLayer(this.clusterMarker)
+    }
+    this.previewMarkers = [];
+}
+
+
+//add previewMarkers to Map
+addMarkers() {
+    this.previewMarkers = this.createMarkersFromTableValue(5);
+
+    if (this.previewMarkers.length === 0) {
+        this.showNoMarkersPopup();
+    } else {
+        if(this.config.isClustering){
+            
+        this.clusterMarker = new MarkerClusterGroup();
+
+        this.previewMarkers.map((singleMarker)=>{
+            this.clusterMarker.addLayer(singleMarker);
+        })
+
+        this.previewMap.addLayer(this.clusterMarker)
+        }else{
+
+            const markerGroup = featureGroup(this.previewMarkers);
+            markerGroup.addTo(this.previewMap)
+        }
+    }
+}
+
+//create previewMarkers from table value
+createMarkersFromTableValue(numOfMarkers) {
+    const tableValue = JSON.parse(this.getAttribute("tableValue"));
+
+    if (tableValue.length === 0) {
+        return [];
+    } else {
+        // TODO ADD CUSTOM ICON SUPPORT
+        let markerIcon;
+        if(this.config.iconurl){
+            markerIcon = icon({
+                iconUrl: this.config.iconurl,
+                iconSize:[32,32]
+            })
+        }else{
+            markerIcon = icon({
+                iconUrl: ICON_URL,
+            })
+        }
+
+        let markers = [];
+
+        for (let index = 0; index < numOfMarkers; index++) {
+          const lat = tableValue[index][this.config.latitudeKey];
+          const lng = tableValue[index][this.config.longitudeKey];
+          markers.push(marker([lat, lng], { icon: markerIcon }).bindPopup(`${this.sampleData.id}, ${lat}, ${lng}`)) ;
+        }
+
+        return markers;
+    }
+}
+
 
   callCustomEvent(data) {
     const event = new CustomEvent("custom-change", {
